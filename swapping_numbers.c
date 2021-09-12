d7ce830b443c2421ea09528bf0eadff68fc367d3
Jidesh <90505732+Jidesh30@users.noreply.github.com>
2021-09-12 14:05:43 +0530

    Update swapping_numbers.c

--- swapping_numbers.c
+++ swapping_numbers.c
@@ -5,16 +5,17 @@ int main()
 {
     //assigning two variables
     int a,b;
-    // asking the user to enter two numbers and it stores in a and b variable.
+    // asking the user to enter two numbers and it stores in a and b variable
     printf("Enter two numbers:\n");
     scanf("%d %d",&a,&b);
-    // Adding both a and b value.
-    a=a+b;
-    // subtracting the current sum value of a from b.
+    // Adding both a and b value
+    a+=b;
+    // subtracting the current sum value of a from b
     b=a-b;
-    // subtracting the a value from the current value of b.
-    a=a-b;
-    // giving the output a and b shows like swapping.
+    // subtracting the a value from the current value of b
+    a-=b;
+    // giving the output a and b shows like swapping
     printf("The swapped numbers are a=%d and b=%d", a,b );
 
     return 0;
+}
